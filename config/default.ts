e94a7c244dfa6b4dfd1565686de8ebaf1369c649
Bart <bartlomiejrybaa@gmail.com>
2021-11-18 09:21:36 +0100

    ADA token implicit, name-> tokenName & Readme update

--- config/default.ts
+++ config/default.ts
@@ -31,11 +31,11 @@ export default {
     address: process.env.MILKOMEDA_ADDRESS || "addr_test1qq6gwl46frfwfk593pqjg39vym476s64n9cmykdl7lv7w32jdqlum6v4sc3l3w9nmuf3ean86n2y7pl83tpxl2qjdw8qmjljz2",
     tokensPerNetwork: [{ 
           name: "testnet",
-          tokens: [ {name: "ADA", policyId: ""}, {name: "TADA", policyId: ""} ] 
+          tokens: [ {tokenName: "TADA", policyId: ""} ] 
         },
         { 
           name: "mainnet",
-          tokens: [ {name: "ADA", policyId: ""} ] 
+          tokens: [] 
         },
       ]
   }
