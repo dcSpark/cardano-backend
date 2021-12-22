7ee6f4e9289dd30962d744ea33c61b2aa511f487
Bart <bartlomiejrybaa@gmail.com>
2021-12-22 15:26:19 +0100

    removing only from tests so all tests are run

--- tests/milkomedaInfo.test.ts
+++ tests/milkomedaInfo.test.ts
@@ -8,7 +8,7 @@ const endpoint = config.apiUrl;
 const minValueToCheck = 10000000;
 const milkomedaAddressToCheck = "addr_test1qq6gwl46frfwfk593pqjg39vym476s64n9cmykdl7lv7w32jdqlum6v4sc3l3w9nmuf3ean86n2y7pl83tpxl2qjdw8qmjljz2";
 
-describe.only("/milkomeda endpoints check", function() {
+describe("/milkomeda endpoints check", function() {
   it("should return list of all Milkomeda parameters", async() => {
     const result = await axios({method: "get", url: endpoint + "milkomeda"});
     expect(result.status).to.be.equal(200);
