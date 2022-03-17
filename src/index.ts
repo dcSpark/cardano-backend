332774639e635257702de1ce2f45774065585eb7
Pran B <15847009+heypran@users.noreply.github.com>
2022-03-17 14:48:47 +0400

    Feat/tx history api version (#38)
    * feat: add api version
    
    * feat: add version comparison
    
    * chore: version bump

--- src/index.ts
+++ src/index.ts
@@ -60,6 +60,11 @@ import { utxoForTransaction } from "./services/utxoForTransaction";
 import { mapTransactionFragsToResponse } from "./utils/mappers";
 
 import promBundle = require("express-prom-bundle");
+
+const TX_HISTORY_API_VERSION = 1;
+// this should be as per the release
+const FLINT_VERSION_WITH_API_VERSION_SUPPORT = "1.8.4";
+
 // for config see: https://www.npmjs.com/package/express-prometheus-middleware
 const metricsMiddleware = promBundle({
   includeStatusCode: true,
@@ -254,6 +259,20 @@ const txHistory = async (req: Request, res: Response) => {
         case "ok": {
           const txs = mapTransactionFragsToResponse(maybeTxs.value);
 
+          if (req.headers?.["flint-version"]) {
+            const userFlintVersion = req.headers?.["flint-version"];
+
+            // https://github.com/substack/semver-compare
+            const flintSupportsApiVersion = semverCompare(
+              userFlintVersion,
+              FLINT_VERSION_WITH_API_VERSION_SUPPORT
+            );
+            // if userFlintVersion >=  FLINT_VERSION_WITH_API_VERSION_SUPPORT
+            if (flintSupportsApiVersion >= 0) {
+              res.send({ txs, version: TX_HISTORY_API_VERSION });
+              return;
+            }
+          }
           res.send(txs);
           return;
         }
